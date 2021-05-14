5463b6baf54ba0bd7fc605415614741f2eacb2ee
Arunima Gupta <arunimagupta71@gmail.com>
2021-05-14 12:19:36 +0530

    Update Single_List_impletation.py

--- Single_List_impletation.py
+++ Single_List_impletation.py
@@ -1,11 +1,11 @@
 class Node:
-def __init__(self,k):
-   self.key=key
-   self.next=None
-temp1=Node(10)
-temp2=Node(20)
-temp3=Node(30)
+  def __init__(self,key):
+    self.key=key
+    self.next=None
+   temp1=Node(10)
+   temp2=Node(20)
+   temp3=Node(30)
 
-temp1.next=temp2
-temp2.next=temp3
-head=temp1
+   temp1.next=temp2
+   temp2.next=temp3
+   head=temp1
